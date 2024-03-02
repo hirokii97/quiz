4f7af9d6d648332bc3b179078d2ff89f23a1b2be
hirokii97 <basket100713hiroki.ch>
2024-03-02 11:53:50 +0900

    feat: 問題の表示

--- index.js
+++ index.js
@@ -18,19 +18,60 @@ const quiz = [
 ];
 
 let quizCount = 0;
+//正解・不正解の判定、表示
+const judge = (e, i,correctBox) => {
+  const correct = quiz[i].correct
+  correctBox.className = `correctBox_${i}`;
+
+  if (correct === e.target.textContent) {
+    correctBox.textContent = "正解";
+  } else {
+    correctBox.textContent = "不正解";
+  }
+};
+
+//quizの数だけ表示する
 const quizLength = quiz.length;
 
 const $button = document.querySelectorAll(".answer");
 const buttonLength = $button.length;
+const quizWrapper = document.getElementById("question-wrapper");
 
 const setQuestion = () => {
   let buttonCount = 0;
   document.getElementById("js-number").textContent = quiz[quizCount].questionNumber;
   document.getElementById("js-question").textContent = quiz[quizCount].question;
+//問題の表示
+for (let i = 0; i < quizLength; i++) {
+  const quizBox = document.createElement("div");
+  quizBox.className = `quiz_${i}`
 
   while (buttonCount < buttonLength) {
     $button[buttonCount].textContent = quiz[quizCount].answers[buttonCount];
     buttonCount++;
+  //クイズ番号
+  const question_number = document.createElement("h2");
+  question_number.className = `question_number_${i + 1}`;
+  question_number.textContent = quiz[i].questionNumber;
+  quizBox.appendChild(question_number);
+
+  //クイズ内容
+  const question = document.createElement("p");
+  question.className = `question_${i + 1}`;
+  question.textContent = quiz[i].question;
+  quizBox.appendChild(question);
+
+  //選択肢
+  const answersBox = document.createElement("div");
+
+  for (let a = 0; a < quiz[i].answers.length; a++) {
+    const answers = document.createElement('button');
+    answers.className = `answers_${i + 1}`;
+    answers.textContent = quiz[i].answers[a];
+    answersBox.appendChild(answers);
+
+    const correctBox = document.createElement("div");
+    });
   }
 };
 setQuestion();
@@ -54,4 +95,7 @@ while (clickedCount < buttonLength) {
     }
   });
   clickedCount++;
+}
+  quizBox.appendChild(answersBox);
+  quizWrapper.appendChild(quizBox);
 }
